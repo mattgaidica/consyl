ae587a4c8d0b9a98beb2a8c8d37bed6ff94f17ec
Matt <matt@primestudiosllc.com>
2012-07-02 12:42:17 -0700

    basic setup, side project

--- app.rb
+++ app.rb
@@ -1,7 +1,25 @@
-print "name: "
-name = gets.chomp
-puts "hello #{name}"
 
-ARGV.each do |arg|
-  puts "#{arg}"
+require "terminal-display-colors"
+#puts "\e[H\e[2J"
+def newline
+  puts "\n"
 end
+def print_and_flush(str)
+  print str
+  $stdout.flush
+end
+
+newline
+puts "service".yellow
+print "  (github) -> "
+service = gets.chomp
+
+puts "task".yellow
+print "  (issue) -> "
+task = gets.chomp
+
+service = "github" unless !service == ""
+task = "issue" unless !task == ""
+
+newline
+puts "Using " + service.green + " to create a " + task.green + "."
